ad99211533c689d5090fcab97d347920b8f25ed5
marcelo <marcelo@marcelo.marcelo>
2018-05-04 19:19:42 -0300

    init

---
+++ rooms_bounded.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+# Variables de entrada
+variaciones = (lambda : np.random.triangular(-20,-10,0), lambda : np.random.triangular(-15, -7.5, 0)
+,lambda : np.random.triangular(-5, -2, 0), lambda : np.random.triangular(0, 1, 5))
+
+# Variables de decision
+
+grado_de_multiplicidad = range(0, 12)
+
+# Parametros
+
+precios = (78, 97, 120, 180)
+
+demandas = (175, 140, 115, 75)
+
+reacciones = (5, 2.5, 0.5, 0.01)
+
+proporciones = (2, 2, 2, 1)
+
+habitaciones_existentes = (200, 200, 200, 100)
+
+cant_repeticiones = 10000
+
+#Calculo de la ganancia
+multiplicidad = 0
+mejor_caso = 0
+for j in grado_de_multiplicidad:
+    ganancias = 0
+    for k in range(0, cant_repeticiones):
+        ganancia = 0
+        for i in range(0, len(variaciones)):
+            precio_variado = variaciones[i]()
+            precio_total = precios[i] * (1 + precio_variado/100)
+            variacion_demanda = reacciones[i] * (-precio_variado) / 100 + 1
+            demanda_total = demandas[i] * variacion_demanda
+            habitaciones_construidas = habitaciones_existentes[i] + j * proporciones[i]
+            habitaciones_alquiladas = min(demanda_total, habitaciones_construidas)
+            ganancia += habitaciones_alquiladas * precio_total
+        ganancias += ganancia
+    if mejor_caso < ganancias / float(cant_repeticiones):
+        mejor_caso = ganancias / float(cant_repeticiones)
+        multiplicidad = j
+    
+print("%d Economico, %d Negocios, %d Ejecutiva, %d Premium,  %f Ganancia" %
+    ( multiplicidad * proporciones[0], multiplicidad * proporciones[1], multiplicidad * proporciones[2], multiplicidad * proporciones[3] , mejor_caso))
+
+            
+    
+
+
+
+
